98b721129591c974aaddb668bb91892314ed65c4
hendisantika <hendisantika@yahoo.co.id>
2026-01-10 07:58:07 +0700

    feat: add ApplicationConstant🫸🌀✏️📗🐧🐳

---
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/constant/ApplicationConstant.java
@@ -0,0 +1,26 @@
+package id.my.hendisantika.reservationservice.constant;
+
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 10/01/26
+ * Time: 07.57
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApplicationConstant {
+    private String errorMsg="is null";
+    private String successMsg;
+}
