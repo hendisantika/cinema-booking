c45c613d1c0d738c0002aeb1049cea2284cce2ef
hendisantika <hendisantika@yahoo.co.id>
2026-01-11 14:44:37 +0700

    feat: Add SeatType 🫸🌀✏️📗🐧🐳

---
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/enums/SeatType.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.reservationservice.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/01/26
+ * Time: 14.44
+ * To change this template use File | Settings | File Templates.
+ */
+public enum SeatType {
+    STANDARD,
+    PREMIUM,
+    VIP,
+    LUXURY,
+    RECLINER,
+    COUPLE,
+    WHEELCHAIR
+}
