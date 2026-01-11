f3db940ea2e0b7f005edfc1d312b50420a255055
hendisantika <hendisantika@yahoo.co.id>
2026-01-11 14:38:03 +0700

    feat: Add PaymentDetails 🫸🌀✏️📗🐧🐳

---
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/dto/PaymentDetails.java
@@ -0,0 +1,39 @@
+package id.my.hendisantika.reservationservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/01/26
+ * Time: 14.37
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentDetails {
+    @NotNull
+    private UUID reservationId;
+    @NotBlank
+    private String customerName;
+    @Email
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String currency;
+    @NotEmpty
+    private List<SeatDetail> seats;
+}
