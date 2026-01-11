d18ea5b1d78037ae45e2dae2f3e47ed7f7d87dd0
hendisantika <hendisantika@yahoo.co.id>
2026-01-11 14:44:20 +0700

    feat: Add ReservationStatus 🫸🌀✏️📗🐧🐳

---
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/enums/ReservationStatus.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.reservationservice.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/01/26
+ * Time: 14.44
+ * To change this template use File | Settings | File Templates.
+ */
+public enum ReservationStatus {
+    PENDING,
+    CONFIRMED,
+    CANCELLED
+}
