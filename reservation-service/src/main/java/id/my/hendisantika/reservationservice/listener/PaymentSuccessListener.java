3c7e08312410214daa6a6ec2e8d528f14eb6e472
hendisantika <hendisantika@yahoo.co.id>
2026-01-11 14:59:43 +0700

    feat: Add PaymentSuccessListener 🫸🌀✏️📗🐧🐳

---
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/listener/PaymentSuccessListener.java
@@ -0,0 +1,29 @@
+package id.my.hendisantika.reservationservice.listener;
+
+import id.my.hendisantika.reservationservice.dto.PaymentSuccessEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/01/26
+ * Time: 14.59
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+@RequiredArgsConstructor
+public class PaymentSuccessListener {
+
+    private final ReservationService reservationService;
+
+    @KafkaListener(topics = "payment.success")
+    public void onPaymentSuccess(PaymentSuccessEvent event) {
+        reservationService.confirmReservation(event.getReservationId());
+    }
+}
