39ddb0323ffa46452ce16e65c3d2ba9c0eb4b8d4
hendisantika <hendisantika@yahoo.co.id>
2026-01-11 14:40:35 +0700

    feat: Add Seat DTO 🫸🌀✏️📗🐧🐳

---
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/dto/Seat.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.reservationservice.dto;
+
+import lombok.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/01/26
+ * Time: 14.40
+ * To change this template use File | Settings | File Templates.
+ */
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Seat {
+    private Long id;
+    private String number;
+    private String rowLetter;
+    private SeatType type;
+    private Double price;
+    private boolean isAvailable;
+    private Long cinemaId;
+
+}
