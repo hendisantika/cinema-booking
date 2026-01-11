fe58c962d35908b572bfc96bc9d5179645a86b64
hendisantika <hendisantika@yahoo.co.id>
2026-01-11 15:13:36 +0700

    feat: Add findAllMovies in MovieService 🫸🌀✏️📗🐧🐳

--- reservation-service/src/main/java/id/my/hendisantika/reservationservice/service/MovieService.java
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/service/MovieService.java
@@ -1,11 +1,17 @@
 package id.my.hendisantika.reservationservice.service;
 
+import id.my.hendisantika.reservationservice.dto.Movie;
+import id.my.hendisantika.reservationservice.entity.MovieEntity;
 import id.my.hendisantika.reservationservice.repository.BranchRepository;
 import id.my.hendisantika.reservationservice.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +31,27 @@ public class MovieService {
     private final MovieRepository movieRepository;
     private final BranchRepository branchRepository;
     private final ModelMapper modelMapper;
+
+    public ResponseEntity<List<Movie>> findAllMovies() {
+        try {
+            List<MovieEntity> movieEntities = movieRepository.findAll();
+            if (movieEntities.isEmpty()) {
+                log.warn("Movie list is empty for the get all movie");
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            }
+            List<Movie> movieList = movieEntities.stream()
+                    .map(entity -> {
+                        Movie movie = modelMapper.map(entity, Movie.class);
+                        movie.setBranchId(entity.getId());
+                        return movie;
+                    })
+                    .toList();
+            log.info("Movie list has been successfully retrieved");
+            return ResponseEntity.status(HttpStatus.OK).body(movieList);
+
+        } catch (Exception ex) {
+            log.error("Exception while finding Branch list: {}", ex.getMessage(), ex);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
