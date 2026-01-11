a6f95f29df86a69d270da9ea64742e713f576d01
hendisantika <hendisantika@yahoo.co.id>
2026-01-11 14:43:13 +0700

    feat: Add MovieResponseDTO 🫸🌀✏️📗🐧🐳

---
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/dto/response/MovieResponseDTO.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.reservationservice.dto.response;
+
+import id.my.hendisantika.reservationservice.dto.Movie;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/01/26
+ * Time: 14.43
+ * To change this template use File | Settings | File Templates.
+ */
+public record MovieResponseDTO(String message, Movie movie) {
+}
