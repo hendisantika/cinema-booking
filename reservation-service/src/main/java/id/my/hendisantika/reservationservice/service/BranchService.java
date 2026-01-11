35c9bf44eacff186a8b9b645e2910995055baaf3
hendisantika <hendisantika@yahoo.co.id>
2026-01-11 15:07:09 +0700

    feat: Add getBranches in BranchService 🫸🌀✏️📗🐧🐳

--- reservation-service/src/main/java/id/my/hendisantika/reservationservice/service/BranchService.java
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/service/BranchService.java
@@ -1,10 +1,16 @@
 package id.my.hendisantika.reservationservice.service;
 
+import id.my.hendisantika.reservationservice.dto.Branch;
+import id.my.hendisantika.reservationservice.entity.BranchEntity;
 import id.my.hendisantika.reservationservice.repository.BranchRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +30,22 @@ public class BranchService {
     private final BranchRepository branchRepository;
     private final ModelMapper modelMapper;
 
+    public ResponseEntity<List<Branch>> getBranches() {
+        try {
+            List<BranchEntity> branchEntities = branchRepository.findAll();
+            if (branchEntities.isEmpty()) {
+                log.warn("No branches found for the add for the cinema");
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            }
+            List<Branch> branchEntityList = branchEntities.stream()
+                    .map(entity -> modelMapper.map(entity, Branch.class))
+                    .toList();
+            log.info("Branches list has been successfully retrieved");
+            return ResponseEntity.status(HttpStatus.OK).body(branchEntityList);
+        } catch (Exception ex) {
+            log.error("Exception while finding Branch list: {}", ex.getMessage(), ex);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+    }
 }
