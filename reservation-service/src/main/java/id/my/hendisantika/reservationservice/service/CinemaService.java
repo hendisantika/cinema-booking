8d4b9790ae1113baa4f930f39b9892500024f90d
hendisantika <hendisantika@yahoo.co.id>
2026-01-11 15:09:47 +0700

    feat: Add getAllCinemas in CinemaService 🫸🌀✏️📗🐧🐳

--- reservation-service/src/main/java/id/my/hendisantika/reservationservice/service/CinemaService.java
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/service/CinemaService.java
@@ -1,11 +1,17 @@
 package id.my.hendisantika.reservationservice.service;
 
+import id.my.hendisantika.reservationservice.dto.Cinema;
+import id.my.hendisantika.reservationservice.entity.CinemaEntity;
 import id.my.hendisantika.reservationservice.repository.BranchRepository;
 import id.my.hendisantika.reservationservice.repository.CinemaRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,4 +33,21 @@ public class CinemaService {
     private final ModelMapper modelMapper;
     private final BranchRepository branchRepository;
 
+    public ResponseEntity<List<Cinema>> getAllCinemas() {
+        try {
+            List<CinemaEntity> cinemaEntities = cinemaRepository.findAll();
+            if (cinemaEntities.isEmpty()) {
+                log.warn("Cinema list is empty for the get all cinemas");
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            }
+            List<Cinema> cinemas = cinemaEntities.stream()
+                    .map(entity -> modelMapper.map(entity, Cinema.class))
+                    .toList();
+            log.info("Cinema list has been successfully retrieved");
+            return ResponseEntity.status(HttpStatus.OK).body(cinemas);
+        } catch (Exception ex) {
+            log.error("Exception while finding Cinemas: {}", ex.getMessage(), ex);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
