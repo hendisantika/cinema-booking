4c9d38984b3dea177d797adfd52d6112a928126b
hendisantika <hendisantika@yahoo.co.id>
2026-01-11 15:06:50 +0700

    feat: Add BranchService 🫸🌀✏️📗🐧🐳

---
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/service/BranchService.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.reservationservice.service;
+
+import id.my.hendisantika.reservationservice.repository.BranchRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/01/26
+ * Time: 15.06
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BranchService {
+    private final BranchRepository branchRepository;
+    private final ModelMapper modelMapper;
+
+}
