b41499bc1714e570723d096c0989d7f3ff8491a2
hendisantika <hendisantika@yahoo.co.id>
2026-01-11 14:55:19 +0700

    feat: Add ReservationSuccessEventProducer 🫸🌀✏️📗🐧🐳

---
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/event/ReservationSuccessEventProducer.java
@@ -0,0 +1,29 @@
+package id.my.hendisantika.reservationservice.event;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/01/26
+ * Time: 14.54
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ReservationSuccessEventProducer {
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void publishReservationSuccess(Notification notification) {
+        kafkaTemplate.send("reservation.success", notification.getReservationId().toString(), notification);
+        log.info("Event Published Successfully by reservationId={}", notification.getReservationId());
+    }
+}
