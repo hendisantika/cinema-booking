d27d988bddd06dbed898b71cbc4aeb6251cb1d02
hendisantika <hendisantika@yahoo.co.id>
2026-01-13 06:31:38 +0700

    feat: Add ReservationSender 🫸🌀✏️📗🐧🐳

---
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/socket/ReservationSender.java
@@ -0,0 +1,33 @@
+package id.my.hendisantika.reservationservice.socket;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 13/01/26
+ * Time: 06.31
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ReservationSender extends TextWebSocketHandler {
+
+    private final ObjectMapper objectMapper;
+    private final Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<>());
+
+}
