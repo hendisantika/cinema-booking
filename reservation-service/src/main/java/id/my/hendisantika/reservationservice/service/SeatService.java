7412ece228cad43817331e29ef4722ca447f026f
hendisantika <hendisantika@yahoo.co.id>
2026-01-13 06:25:13 +0700

    feat: Add addSeat in SeatService 🫸🌀✏️📗🐧🐳

--- reservation-service/src/main/java/id/my/hendisantika/reservationservice/service/SeatService.java
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/service/SeatService.java
@@ -1,10 +1,15 @@
 package id.my.hendisantika.reservationservice.service;
 
+import id.my.hendisantika.reservationservice.dto.Seat;
+import id.my.hendisantika.reservationservice.dto.response.SeatResponseDTO;
+import id.my.hendisantika.reservationservice.entity.SeatEntity;
 import id.my.hendisantika.reservationservice.repository.CinemaRepository;
 import id.my.hendisantika.reservationservice.repository.SeatRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 /**
@@ -25,4 +30,31 @@ public class SeatService {
     private final SeatRepository seatRepository;
     private final ModelMapper modelMapper;
     private final CinemaRepository cinemaRepository;
+
+    public ResponseEntity<SeatResponseDTO> addSeat(Seat seatRequest) {
+        try {
+            if (seatRequest == null) {
+                log.warn("seat is null for add seat");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            }
+            if (seatRepository.existsByNumber(seatRequest.getNumber()) &&
+                    seatRepository.existsByRowLetter(seatRequest.getRowLetter())) {
+                log.warn("seat is already exist for add seat");
+                return ResponseEntity.status(HttpStatus.CONFLICT).build();
+            }
+            SeatEntity seatEntity = modelMapper.map(seatRequest, SeatEntity.class);
+            seatEntity.setCinema(cinemaRepository.findById(seatRequest.getCinemaId()).orElse(null));
+
+            SeatEntity saved = seatRepository.save(seatEntity);
+            Seat seat = modelMapper.map(saved, Seat.class);
+            seat.setCinemaId(saved.getId());
+            log.info("seat added successfully for add seat");
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(new SeatResponseDTO("Seat successfully added", seat));
+
+        } catch (Exception ex) {
+            log.error("Exception while adding Seat {}", ex.getMessage(), ex);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
