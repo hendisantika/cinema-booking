907c32e6c6d623e29c996d402f5142900791649a
hendisantika <hendisantika@yahoo.co.id>
2026-01-13 06:19:01 +0700

    feat: Add setReservation in ReservationService 🫸🌀✏️📗🐧🐳

--- reservation-service/src/main/java/id/my/hendisantika/reservationservice/service/ReservationService.java
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/service/ReservationService.java
@@ -1,9 +1,12 @@
 package id.my.hendisantika.reservationservice.service;
 
+import id.my.hendisantika.reservationservice.dto.PaymentDetails;
 import id.my.hendisantika.reservationservice.dto.Reservation;
+import id.my.hendisantika.reservationservice.dto.SeatDetail;
 import id.my.hendisantika.reservationservice.dto.response.ReservationResponseDTO;
-import id.my.hendisantika.reservationservice.entity.ReservationEntity;
-import id.my.hendisantika.reservationservice.entity.SeatEntity;
+import id.my.hendisantika.reservationservice.entity.*;
+import id.my.hendisantika.reservationservice.enums.ReservationStatus;
+import id.my.hendisantika.reservationservice.enums.SeatType;
 import id.my.hendisantika.reservationservice.event.ReservationCreatedEventProducer;
 import id.my.hendisantika.reservationservice.event.ReservationSuccessEventProducer;
 import id.my.hendisantika.reservationservice.repository.*;
@@ -14,9 +17,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.ResourceAccessException;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -83,6 +87,121 @@ public class ReservationService {
             log.info("No reservation with id: {}", reservationId);
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(new ReservationResponseDTO("reservation with " + reservationId + " not exist ", null));
+        } catch (Exception e) {
+            log.error("Exception while finding user: {}", e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @Transactional
+    public ResponseEntity<ReservationResponseDTO> setReservation(Reservation reservationRequestDTO) {
+        try {
+            if (reservationRequestDTO == null) {
+                log.error("reservations is null for the add");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            }
+            CustomerEntity customerEntity = customerRepository.findById(reservationRequestDTO.getCustomerId())
+                    .orElseThrow(() -> new RuntimeException("Customer not found"));
+
+            CinemaEntity cinemaEntity = cinemaRepository.findById(reservationRequestDTO.getCinemaId())
+                    .orElseThrow(() -> new RuntimeException("Cinema not found"));
+
+            MovieEntity movieEntity = movieRepository.findById(reservationRequestDTO.getMovieId())
+                    .orElseThrow(() -> new RuntimeException("Movie not found"));
+
+            List<SeatEntity> seatEntities = seatRepository.findAllById(reservationRequestDTO.getSeatIds());
+
+            if (seatEntities.size() != reservationRequestDTO.getSeatIds().size()) {
+                log.error("Some seats not found");                        //check the all seat in the hall
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body(new ReservationResponseDTO("Some seats not found", null));
+            }
+            for (SeatEntity seatEntity : seatEntities) {
+                if (!seatEntity.isAvailable()) {
+                    log.error("Seat {}{} not available", seatEntity.getRowLetter(), seatEntity.getNumber());
+                    return ResponseEntity.status(HttpStatus.CONFLICT)
+                            .body(new ReservationResponseDTO("Seat " + seatEntity.getRowLetter() + seatEntity.getNumber() + " not available", null));
+                }
+            }
+            ReservationEntity reservationEntity = new ReservationEntity();
+            reservationEntity.setConNumber(reservationRequestDTO.getConNumber());
+            reservationEntity.setDescription(reservationRequestDTO.getDescription());
+            reservationEntity.setDate(reservationRequestDTO.getDate());
+            reservationEntity.setTime(reservationRequestDTO.getTime());
+            reservationEntity.setCustomer(customerEntity);
+            reservationEntity.setCinema(cinemaEntity);
+            reservationEntity.setStatus(ReservationStatus.PENDING);
+            reservationEntity.setMovie(movieEntity);
+
+
+            // Set<SeatEntity> seats=new HashSet<>();
+            for (SeatEntity seatEntity : seatEntities) {
+                seatEntity.setAvailable(false);
+                seatEntity.getReservation().add(reservationEntity);
+                reservationEntity.getSeats().add(seatEntity);
+            }
+
+            ReservationEntity savedReservation = reservationRepository.save(reservationEntity);
+            log.info("Created reservation with id : {}", reservationEntity.getReservationId());
+
+
+            Reservation reservation = new Reservation();
+            reservation.setReservationId(savedReservation.getReservationId());
+            reservation.setConNumber(savedReservation.getConNumber());
+            reservation.setDescription(savedReservation.getDescription());
+            reservation.setDate(savedReservation.getDate());
+            reservation.setTime(savedReservation.getTime());
+            reservation.setCinemaId(cinemaEntity.getId());
+            reservation.setMovieId(movieEntity.getId());
+            reservation.setCustomerId(customerEntity.getId());
+
+            List<Long> seatIds = savedReservation.getSeats()
+                    .stream().map(SeatEntity::getId).collect(Collectors.toList());
+            reservation.setSeatIds(seatIds);
+
+            sender.broadcast(new ReservationResponseDTO("Reservation made successfully", reservation));
+
+            List<SeatDetail> seatDetails = new ArrayList<>();
+            PaymentDetails paymentDetails = new PaymentDetails();
+            paymentDetails.setReservationId(reservationEntity.getReservationId());
+            paymentDetails.setCustomerName(reservationEntity.getCustomer().getName());
+            paymentDetails.setEmail(reservationEntity.getCustomer().getEmail());
+            paymentDetails.setCurrency("LKR");
+
+            Map<SeatType, Integer> seatCountMap = new EnumMap<>(SeatType.class);
+            Map<SeatType, Double> seatUnitPriceMap = new EnumMap<>(SeatType.class);
+
+            for (SeatEntity seat : reservationEntity.getSeats()) {
+                SeatType seatType = seat.getType();
+                Double price = seat.getPrice();
+
+                seatCountMap.put(seatType, seatCountMap.getOrDefault(seatType, 0) + 1);
+
+                if (seatUnitPriceMap.containsKey(seatType)) {
+                    if (!seatUnitPriceMap.get(seatType).equals(price)) {
+                        throw new IllegalStateException("price mismatch for seat type: " + seatType);
+                    }
+                } else {
+                    seatUnitPriceMap.put(seatType, price);
+                }
+            }
+            for (SeatType type : seatCountMap.keySet()) {
+                SeatDetail seatDetail = new SeatDetail();
+                seatDetail.setType(type);
+                seatDetail.setQuantity(seatCountMap.get(type));
+                seatDetail.setPrice(seatUnitPriceMap.get(type));
+                seatDetails.add(seatDetail);
+            }
+            paymentDetails.setSeats(seatDetails);
+
+            reservationCreatedEventProducer.publishReservationCreated(paymentDetails);
+
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(new ReservationResponseDTO("Reservation made successfully", reservation));
+
+        } catch (ResourceAccessException ex) {
+            log.error("Resource access exception: {}", ex.getMessage(), ex);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         } catch (Exception e) {
             log.error("Exception while finding user: {}", e.getMessage(), e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
