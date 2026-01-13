d08a72e606e3a42a49b15f99597267a0f7ec3c16
hendisantika <hendisantika@yahoo.co.id>
2026-01-14 06:57:41 +0700

    feat: Add Session🫸🌀✏️📗🐧🐳

---
+++ payment-service/src/main/java/id/my/hendisantika/paymentservice/session/Session.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.paymentservice.session;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 14/01/26
+ * Time: 06.57
+ * To change this template use File | Settings | File Templates.
+ */
+public class Session {
+}
