bfcf424f45482cb2c6773f7a88bfb7aacbc71db7
hendisantika <hendisantika@yahoo.co.id>
2026-01-13 07:56:23 +0700

    feat: Add addSeat in SeatController 🫸🌀✏️📗🐧🐳

--- reservation-service/src/main/java/id/my/hendisantika/reservationservice/controller/SeatController.java
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/controller/SeatController.java
@@ -1,7 +1,13 @@
 package id.my.hendisantika.reservationservice.controller;
 
+import id.my.hendisantika.reservationservice.dto.Seat;
+import id.my.hendisantika.reservationservice.dto.response.SeatResponseDTO;
 import id.my.hendisantika.reservationservice.service.SeatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +28,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class SeatController {
     private final SeatService seatService;
 
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<SeatResponseDTO> addSeat(@RequestBody Seat seat) {
+        return seatService.addSeat(seat);
+    }
 }
