eab204f1b9b622dbdfc59651a1d288d8c08fdbd2
hendisantika <hendisantika@yahoo.co.id>
2026-01-13 07:09:52 +0700

    feat: Add setCustomer in CustomerController 🫸🌀✏️📗🐧🐳

--- reservation-service/src/main/java/id/my/hendisantika/reservationservice/controller/CustomerController.java
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/controller/CustomerController.java
@@ -1,14 +1,14 @@
 package id.my.hendisantika.reservationservice.controller;
 
 import id.my.hendisantika.reservationservice.dto.Customer;
+import id.my.hendisantika.reservationservice.dto.response.CustomerResponseDTO;
 import id.my.hendisantika.reservationservice.service.CustomerService;
 import id.my.hendisantika.reservationservice.service.UserSyncService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,4 +36,11 @@ public class CustomerController {
     public ResponseEntity<List<Customer>> getAllCustomer() {
         return customerService.getAllCustomer();
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping
+    public ResponseEntity<CustomerResponseDTO> setCustomer(@Valid @RequestBody Customer customer) {
+        return customerService.setCustomer(customer);
+    }
+
 }
