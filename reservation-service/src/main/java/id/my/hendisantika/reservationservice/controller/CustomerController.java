c223a7377f7d189c98ce0e543c91ceb22bec6d0e
hendisantika <hendisantika@yahoo.co.id>
2026-01-13 07:09:22 +0700

    feat: Add getAllCustomer 🫸🌀✏️📗🐧🐳

--- reservation-service/src/main/java/id/my/hendisantika/reservationservice/controller/CustomerController.java
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/controller/CustomerController.java
@@ -1,10 +1,16 @@
 package id.my.hendisantika.reservationservice.controller;
 
+import id.my.hendisantika.reservationservice.dto.Customer;
 import id.my.hendisantika.reservationservice.service.CustomerService;
 import id.my.hendisantika.reservationservice.service.UserSyncService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +30,10 @@ public class CustomerController {
 
     private final CustomerService customerService;
     private final UserSyncService userSyncService;
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomer() {
+        return customerService.getAllCustomer();
+    }
 }
