f22f997c748e2bd6d3162be99cc1796cf3162b48
hendisantika <hendisantika@yahoo.co.id>
2026-01-14 06:41:09 +0700

    feat: Add notification-service🫸🌀✏️📗🐧🐳

---
+++ notification-service/src/main/java/id/my/hendisantika/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+}
