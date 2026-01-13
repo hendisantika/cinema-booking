28dd94ead7ab66c064468ce61c5b3d0e46e6f7f9
hendisantika <hendisantika@yahoo.co.id>
2026-01-14 06:50:17 +0700

    feat: Add EmailService🫸🌀✏️📗🐧🐳

---
+++ notification-service/src/main/java/id/my/hendisantika/notificationservice/service/EmailService.java
@@ -0,0 +1,32 @@
+package id.my.hendisantika.notificationservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 14/01/26
+ * Time: 06.49
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+        mailSender.send(message);
+
+    }
+}
