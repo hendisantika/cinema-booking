1bbf460e3dd2f0c33a140ac3af4f648cfa9fc076
hendisantika <hendisantika@yahoo.co.id>
2026-01-13 07:11:58 +0700

    feat: Add getMovieById in MovieController 🫸🌀✏️📗🐧🐳

--- reservation-service/src/main/java/id/my/hendisantika/reservationservice/controller/MovieController.java
+++ reservation-service/src/main/java/id/my/hendisantika/reservationservice/controller/MovieController.java
@@ -1,7 +1,12 @@
 package id.my.hendisantika.reservationservice.controller;
 
+import id.my.hendisantika.reservationservice.dto.response.MovieResponseDTO;
 import id.my.hendisantika.reservationservice.service.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +27,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovieController {
 
     private final MovieService movieService;
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<MovieResponseDTO> getMovieById(@PathVariable Long id) {
+        return movieService.findMovieById(id);
+    }
 }
