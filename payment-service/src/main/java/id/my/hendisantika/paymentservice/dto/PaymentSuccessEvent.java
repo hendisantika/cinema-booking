46f9a4c69c837cc6caac460ee27b9fa475aaa22d
hendisantika <hendisantika@yahoo.co.id>
2026-01-14 07:00:34 +0700

    feat: Add PaymentSuccessEvent DTO🫸🌀✏️📗🐧🐳

---
+++ payment-service/src/main/java/id/my/hendisantika/paymentservice/dto/PaymentSuccessEvent.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.paymentservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 14/01/26
+ * Time: 07.00
+ * To change this template use File | Settings | File Templates.
+ */
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentSuccessEvent {
+    private UUID reservationId;
+    private String paymentIntentId;
+    private Long amountTotal;
+    private String currency;
+}
