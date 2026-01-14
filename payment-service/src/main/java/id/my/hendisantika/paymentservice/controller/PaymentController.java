0ccc78b0501d1c83e180bf5f25aa2fe02c766caf
hendisantika <hendisantika@yahoo.co.id>
2026-01-14 07:07:29 +0700

    feat: Add handleWebhook in PaymentController🫸🌀✏️📗🐧🐳

--- payment-service/src/main/java/id/my/hendisantika/paymentservice/controller/PaymentController.java
+++ payment-service/src/main/java/id/my/hendisantika/paymentservice/controller/PaymentController.java
@@ -1,10 +1,18 @@
 package id.my.hendisantika.paymentservice.controller;
 
+import com.stripe.model.Event;
+import com.stripe.model.PaymentIntent;
+import com.stripe.net.Webhook;
+import id.my.hendisantika.paymentservice.dto.PaymentFailedEvent;
+import id.my.hendisantika.paymentservice.dto.PaymentSuccessEvent;
+import id.my.hendisantika.paymentservice.session.Session;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,6 +30,74 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PaymentController {
     private final KafkaTemplate<String, Object> kafkaTemplate;
+
     @Value("${stripe.webhookSecret}")
     private String webhookSecret;
+
+    @PostMapping
+    public ResponseEntity<String> handleWebhook(
+            @RequestBody String payload,
+            @RequestHeader("Stripe-Signature") String sigHeader
+
+    ) {
+        Event event;
+
+        try {
+            event = Webhook.constructEvent(
+                    payload,
+                    sigHeader,
+                    webhookSecret
+            );
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Invalid signature");
+        }
+        switch (event.getType()) {
+
+            case "checkout.session.completed" -> {
+                Session session = (Session) event.getDataObjectDeserializer()
+                        .getObject().orElseThrow();
+
+                String reservationIdStr = session.getMetadata().get("reservationId");
+
+                if (reservationIdStr == null || reservationIdStr.isBlank()) {
+                    throw new IllegalStateException("Missing reservationId in Stripe metadata");
+                }
+                UUID reservationId = UUID.fromString(reservationIdStr);
+
+                kafkaTemplate.send(
+                        "payment.success",
+                        new PaymentSuccessEvent(
+                                reservationId,
+                                session.getPaymentIntent(),
+                                session.getAmountTotal(),
+                                session.getCurrency()
+                        )
+                );
+            }
+
+            case "payment_intent.payment_failed",
+                 "checkout.session.async_payment_failed" -> {
+
+                PaymentIntent intent = (PaymentIntent) event
+                        .getDataObjectDeserializer()
+                        .getObject().orElseThrow();
+
+                String reservationIdStr = intent.getMetadata().get("reservationId");
+
+                UUID reservationId = UUID.fromString(reservationIdStr);
+
+                kafkaTemplate.send(
+                        "payment.failed",
+                        new PaymentFailedEvent(
+                                reservationId,
+                                intent.getId(),
+                                intent.getLastPaymentError() != null
+                                        ? intent.getLastPaymentError().getMessage()
+                                        : "PAYMENT_FAILED"
+                        )
+                );
+            }
+        }
+        return ResponseEntity.ok("webhook processed");
+    }
 }
